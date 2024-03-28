649dc18db747e21ca9b2419203b7095a2163df21
Francis <francisbernal14@gmail.com>
2024-03-28 16:28:55 +0100

    Create first definitions and fetch api function to get all movies

---
+++ src/utils/definitions.ts
@@ -0,0 +1,38 @@
+export interface LinkButton {
+    link: string;
+    value: string;
+    position?: string;
+}
+
+export interface getMoviesResponse {
+    page: number
+    results: Movie[]
+    total_pages: number
+    total_results: number
+  }
+  
+  export interface Movie {
+    adult: boolean
+    backdrop_path: string
+    genre_ids: number[]
+    id: number
+    original_language: string
+    original_title: string
+    overview: string
+    popularity: number
+    poster_path: string
+    release_date: string
+    title: string
+    video: boolean
+    vote_average: number
+    vote_count: number
+  }
+
+export interface MovieCard {
+  title: string
+  year: string
+  rating: number
+  poster_path: string
+  genre: string
+}
+  
