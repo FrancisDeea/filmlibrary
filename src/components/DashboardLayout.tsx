436d2e4a9763aea52c62d2d5968daa26ee7a03da
Francis <francisbernal14@gmail.com>
2024-04-03 15:21:41 +0200

    fix header style and adjust general padding layout

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -7,16 +7,19 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="h-full border-2 border-gray-700 rounded-lg overflow-hidden">
-      <header className="w-full bg-[#27272a] border-b-2 border-b-gray-700 h-12 ">
-        A-Safe Digital Technical Test - Filmlibrary by Francis Bernal
+    <div className="h-full border-2 border-gray-700 rounded-lg overflow-hidden max-sm:border-none">
+      <header className="p-2 w-full bg-[#27272a] border-b-2 border-b-gray-700 min-h-12 flex flex-wrap justify-center items-center gap-2">
+        <p className="font-bold text-xl">Movie Library</p>
+        <span className="font-normal italic text-base">
+          a technical test for A-Safe Digital
+        </span>
       </header>
       <div className="flex flex-row h-full">
         <aside className="w-60 border-r-2 border-r-gray-700 max-sm:hidden">
           <SideNav />
         </aside>
         <MobileMenu />
-        <main className="flex-1 h-[calc(100%-48px)]">{children}</main>
+        <main className="flex-1 h-[calc(100%-48px)] py-4">{children}</main>
       </div>
     </div>
   );
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -4,7 +4,7 @@ import type { Movie, MovieCard } from "@/lib/definitions";
 import { categoryDictionary } from "@/lib/utils";
 
 export default function MovieCard(props: Movie) {
-  const basePathImages = "https://image.tmdb.org/t/p/w342";
+  const basePathImages = "https://image.tmdb.org/t/p/w500";
   const {
     title,
     release_date: year,
@@ -17,7 +17,7 @@ export default function MovieCard(props: Movie) {
   const genres = genre_ids.slice(0, 2);
 
   return (
-    <article className="bg-pink-400 w-full max-w-64 max-sm:max-w-max rounded-lg overflow-hidden relative cursor-pointer transition-all hover:scale-105 hover:saturate-150">
+    <article className="bg-pink-400 w-full max-w-64 max-sm:max-w-max rounded-lg overflow-hidden relative cursor-pointer transition-all hover:scale-95 hover:saturate-150">
       <div className="absolute top-0 bottom-0 h-full w-full bg-gradient-to-b from-transparent from-50% via-black/60 to-black/80"></div>
       <div className="absolute top-2 left-2 bg-black rounded-md px-3 py-1">
         {formattedRating}
