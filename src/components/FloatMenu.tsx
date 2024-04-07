a27fe5b8628970e6f483bf7487244591e4c33e22
Francis <francisbernal14@gmail.com>
2024-04-07 05:37:00 +0200

    add suspense to search float menu

--- src/components/FloatMenu.tsx
+++ src/components/FloatMenu.tsx
@@ -1,22 +1,24 @@
-import Link from "next/link";
 import Search from "./Search";
-import { BackIcon, MenuIcon } from "@/lib/icons";
-import { useRouter } from "next/navigation";
 import BackButton from "./BackButton";
+import { MenuIcon } from "@/lib/icons";
+import { Suspense } from "react";
 
 export default function FloatMenu() {
-  const router = useRouter;
   return (
     <>
       <nav className="absolute bottom-10 left-0 right-0 w-full max-w-52 h-8 m-auto flex bg-primary rounded-3xl text-white shadow-2xl z-20">
         <BackButton />
+
         <label
           htmlFor="modalMenu"
           className="flex justify-center items-center w-full h-full text-center cursor-pointer flex-1"
         >
           <MenuIcon style="size-6" />
         </label>
-        <Search key="mobile" placeholder="Search a movie by title..." />
+
+        <Suspense>
+          <Search key="mobile" placeholder="Search a movie by title..." />
+        </Suspense>
       </nav>
     </>
   );
